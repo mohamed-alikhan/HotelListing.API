151c794002e4aa12890dfbdffefe77ae067721dd
Mohamed Alikhan M <mmdalikhan@gmail.com>
2024-06-23 23:03:48 -0700

    implementing AutoMapper in POST method

---
+++ HotelListing.API/Configurations/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HotelListing.API.Data;
+using HotelListing.API.Models.Country;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+
+namespace HotelListing.API.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<Country, CreateCountryDto>().ReverseMap();
+        }
+    }
+}
